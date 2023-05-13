8952ce4ed2b13be5f051c231618eafec2a5dbd3b
juanccalzadilla <juancalza91@gmail.com>
2023-05-13 18:47:34 +0200

    #5 #6 Seeders - Creación de seeders para tabla users y articles

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::truncate();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('adminadmin'),
+        ]);
+
+        User::factory(10)->create();
+    }
+}
