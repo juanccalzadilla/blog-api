f245c8edaee9019c513c4ae5a4535d07142237b7
juanccalzadilla <juancalza91@gmail.com>
2023-05-15 13:30:28 +0200

    #16 Relación de tablas - Creada la relación entre user y article - Modificado el seeder de article - Agregado el modelo Comments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(ArticlesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(ArticlesTableSeeder::class);
     }
 }
